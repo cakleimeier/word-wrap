e26c92431c59adc9424677d6c7e33494b42c8b1d
Catherine Kleimeier <catherine@masugadesign.com>
2021-01-27 18:11:16 -0500

    Add login to createWrappedString function

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -15,6 +15,7 @@ class FormController extends Controller
      */
     public function createWrappedString(Request $request)
     {
+        # NOTE: not using `wordwrap` function because that might be cheating?
         $string = $request->string ? (string) $request->string : null;
         $number = $request->number ? (int) $request->number : null;
         $response = '';
@@ -22,15 +23,81 @@ class FormController extends Controller
         # If there is no string or number, return nothing
         if (!$string || !$number) {
             $reponse = 'Missing parameter';
-            return ['response' => $response];
         } elseif ((gettype($string) != 'string') || (gettype($number) != 'integer')) {
             $response = 'Invalid parameter';
-            return ['response' => $response];
         }
 
         # If the string is too short to be split, just return it
         if (strlen($string) <= $number) {
-            return ['response' => $string];
+            $response = $string;
+        } else {
+            # Split the string at the whitespace
+            $splitString = explode(' ', $string);
+            # Create the new string array
+            $newStringArray = [];
+            # Keep track of the loop index
+            $loopIndex = 0;
+            # Keep track of the total line length
+            $lineLength = 0;
+            # Keep track of the number of lines
+            $totalLines = 0;
+
+            # Create the split array, newString
+            foreach ($splitString as $key => $value) {
+                # Get the length of the current word
+                $wordLength = strlen($value);
+
+                # Calculate the total length of the line,
+                # including the spaces between words, as
+                # represented by loopIndex
+                $totalLength = $wordLength + $lineLength + $loopIndex;
+
+                # If that's not too long, add it to the new string
+                if ($totalLength <= $number) {
+                    if (!array_key_exists($totalLines, $newStringArray)) {
+                        $newStringArray[$totalLines] = [];
+                    }
+                    array_push($newStringArray[$totalLines], $splitString[$key]);
+
+                    $loopIndex++;
+                    $lineLength = $totalLength;
+
+                # If it is too long, add it to the next array
+                # and recalculate variables
+                } else {
+                    $totalLines++;
+
+                    $newStringArray[$totalLines] = [];
+                    array_push($newStringArray[$totalLines], $splitString[$key]);
+
+                    $loopIndex = 1;
+                    $lineLength = $wordLength + $loopIndex;
+                }
+            }
+
+            # Create the new string by adding each word
+            # and then each line to the string
+            $newString = '';
+            foreach ($newStringArray as $lineKey => $lineElement) {
+                foreach ($lineElement as $wordKey => $wordElement) {
+                    $newString .= $wordElement;
+
+                    # If this isn't the last word in the line,
+                    # add a space after it
+                    if ($wordKey != array_key_last($lineElement)) {
+                        $newString .= ' ';
+                    }
+                }
+
+                # If this isn't the last line, add a break
+                if ($lineKey != array_key_last($newStringArray)) {
+                    $newString .= '<br>';
+                }
+            }
+
+            $response = $newString;
         }
+
+        return ['response' => $response];
     }
 }
