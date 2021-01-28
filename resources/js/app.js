e86570733e117c4286087b8b28e96661fe4f92d6
Catherine Kleimeier <catherine@masugadesign.com>
2021-01-28 18:44:42 -0500

    Import Vue components to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import Form from './components/WordWrapForm.vue';
+import Output from './components/FormOutput.vue';
+
+Vue.component(
+	'word-wrap-form-component',
+	require('./components/WordWrapForm.vue').default
+);
+Vue.component(
+	'form-output-component',
+	require('./components/FormOutput.vue').default
+);
+
+var app = new Vue({
+	el: '#app'
+});
