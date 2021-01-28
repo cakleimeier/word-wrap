970ab63064899baa04809dad28b9a47a614c0763
Catherine Kleimeier <catherine@masugadesign.com>
2021-01-28 18:44:04 -0500

    Create FormOutput.vue

---
+++ resources/js/components/FormOutput.vue
@@ -0,0 +1,26 @@
+<!-- Output Component
+    Receives parsed string for display
+-->
+
+<template>
+    <article class="inner-wrapper">
+        <h2>View the Results</h2>
+        <div id="output-container">
+            <span v-html="parsedString"></span>
+        </div>
+    </article>
+</template>
+
+<script>
+    export default {
+        name    : 'form-output',
+        data    : function() {
+            return {}
+        },
+        props   : {
+            parsedString: String
+        },
+        methods : {
+        }
+    }
+</script>
